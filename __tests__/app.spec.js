45fc7c59d217ee8760d25508a6d8d0642f3aa2cb
NegativeRainbow <danishb@uw.edu>
2017-10-27 11:19:07 -0700

    Did some work in lab

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -3,4 +3,23 @@
 import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
+//{Feature} should {test}
+describe('Basic Math', function(){
+    //tests
+    it('Should add numbers correctly', function(){
+        //javascript
+        let sum = 1+1;
 
+        //assertion
+        expect(sum).toEqual(2);
+    })
+})
+
+describe('InvertCase should invert case', function(){
+    it('Should invert the tag xXxSniPerELitExXx', function(){
+        let string = 'xXxSniPerELitExXx';
+        string = invertCase(string);
+
+        expect(string).toEqual('XxXsNIpERelITeXxX');
+    })
+})
